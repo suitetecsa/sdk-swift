5e95147c5b1ed0a2499ea47fef89bafd2f8db4cd
lesclaz <lesclaz95@gmail.com>
2024-03-24 21:07:24 -0400

    'DataSession' initializer is inaccessible due to 'internal' protection Fixes #1

--- Sources/SuitEtecsaSdk/entity/data_session.swift
+++ Sources/SuitEtecsaSdk/entity/data_session.swift
@@ -3,4 +3,11 @@ public struct DataSession {
   public let csrfHw: String
   public let wlanUserIp: String
   public let attributeUUID: String
+
+  public init(username: String, csrfHw: String, wlanUserIp: String, attributeUUID: String) {
+    self.username = username
+    self.csrfHw = csrfHw
+    self.wlanUserIp = wlanUserIp
+    self.attributeUUID = attributeUUID
+  }
 }
