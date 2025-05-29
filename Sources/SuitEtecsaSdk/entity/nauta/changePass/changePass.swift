87055a23da684ff46e3d704696c17544e2a93df9
Pedro Omar <126366180+pedroomar23@users.noreply.github.com>
2025-05-28 21:01:57 -0400

    Update changePass.swift
    Co-authored-by: lesclaz <lesclaz95@gmail.com>

--- Sources/SuitEtecsaSdk/entity/nauta/changePass/changePass.swift
+++ Sources/SuitEtecsaSdk/entity/nauta/changePass/changePass.swift
@@ -7,13 +7,3 @@ struct ChangePass: Codable, Sendable {
         case param = "param"
     }
 }
-
-public struct Param: Codable, Sendable {
-    public var name: String 
-    public var value: String 
-
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case value = "value"
-    }
-}
