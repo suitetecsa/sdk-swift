9683129c0cd1cf870beefa55b26d176f34b6e49f
Pedro Omar <126366180+pedroomar23@users.noreply.github.com>
2025-05-28 21:04:25 -0400

    Update validateUser.swift
    Co-authored-by: lesclaz <lesclaz95@gmail.com>

--- Sources/SuitEtecsaSdk/entity/nauta/register/validateUser.swift
+++ Sources/SuitEtecsaSdk/entity/nauta/register/validateUser.swift
@@ -7,13 +7,3 @@ struct ValidateUser: Codable, Sendable {
         case param = "param"
     }
 }
-
-public struct Parans: Codable, Sendable {
-    public var name: String 
-    public var value: String 
-
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case value = "value"
-    }
-}
