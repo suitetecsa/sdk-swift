60dd64aae59d0416d7ec1bb60ca591cc606b73c1
Pedro Omar <126366180+pedroomar23@users.noreply.github.com>
2025-05-28 21:03:54 -0400

    Update registerUser.swift
    Co-authored-by: lesclaz <lesclaz95@gmail.com>

--- Sources/SuitEtecsaSdk/entity/nauta/register/registerUser.swift
+++ Sources/SuitEtecsaSdk/entity/nauta/register/registerUser.swift
@@ -7,13 +7,3 @@ struct RegisterUser: Codable, Sendable {
         case param = "param"
     }
 }
-
-public struct Paramsssss: Codable, Sendable {
-    public var name: String 
-    public var value: String 
-
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case value = "value"
-    }
-}
